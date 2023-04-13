7bc2097e61262e44064ef12ae29d7d08b6c971c3
Anna Rönnfeldt <roennfeldt@ibb.uni-potsdam.de>
2023-04-13 10:49:52 +0200

    renamed script to indicate native focus

--- scripts/05c_background_thinning_nat.R
+++ scripts/05c_background_thinning_nat.R
@@ -3,6 +3,7 @@
 # library(sf)
 # library(terra)
 # library(tidyverse)
+# library(sfheaders)
 
 # preamble
 rm(list = ls())
@@ -14,7 +15,7 @@ install.load.package <- function(x) {
   require(x, character.only = TRUE)
 }
 package_vec <- c(
-  "dplyr", "sf", "doParallel", "foreach", "terra", "purrr", "furrr" # names of the packages required placed here as character objects
+  "dplyr", "sf", "doParallel", "foreach", "terra", "purrr", "furrr", "sfheaders" # names of the packages required placed here as character objects
 )
 
 sapply(package_vec, install.load.package)
@@ -22,8 +23,6 @@ sapply(package_vec, install.load.package)
 # required paths
 # path_ds <- "Z:/AG26/Arbeit/datashare/data/biodat/distribution/Pacific_invaders/"
 path_imp <- file.path("/import/ecoc9z/data-zurell/roennfeldt/C1/")
-
-
 
 # load functions
 # required in this script: thin
@@ -71,8 +70,10 @@ thin <- function(sf, thin_dist = 5000, runs = 10, ncores = 10){
   
   out <- sf[selected_run,]
   
-  out
+  out <- sf_to_df(out)[,3:4] %>%
+    rename("lon" = "x", "lat" = "y")
   
+  out
 }
 
 
@@ -85,16 +86,16 @@ load(paste0(path_imp, "input_data/enough_occs.RData"))
 
 # get names of relevant species ------------------------------------------------
 # species suitable for the analysis (>= 20 occs over the island region)
-spp <- occ_enough[["spp_names"]][["general"]]
-spp <- spp[1:10]
+spp <- occ_enough[["spp_names"]][["general"]][431:506]
+
 
 
 # Start parallel computing
-no_cores <- 4
+no_cores <- 15
 cl <- makeCluster(no_cores)
 registerDoParallel(cl)
 
-foreach(spp_index = 1:length(spp), .packages = c("terra", "tidyverse", "sf", "purrr", "furrr")) %dopar% {
+foreach(spp_index = 1:length(spp), .packages = c("terra", "tidyverse", "sf", "purrr", "furrr", "sfheaders")) %dopar% {
   try({
     
     world_mask <- rast(paste0(path_imp, "input_data/world_mask.tif"))
@@ -119,14 +120,14 @@ foreach(spp_index = 1:length(spp), .packages = c("terra", "tidyverse", "sf", "pu
     buf_500 <- buffer(presences, width = 500000)
     
     # from here on each step is done twice (for the two different buffer sizes respectively)
-
+    
     # create mask
     mask_buf_200 <- crop(world_mask, ext(buf_200))
     values(mask_buf_200)[!is.na(values(mask_buf_200))] <- 1
     
     mask_buf_500 <- crop(world_mask, ext(buf_500))
     values(mask_buf_500)[!is.na(values(mask_buf_500))] <- 1
-    
+
     # use mask_buf to rasterize buf (which has been a vector so far; !raster required for later steps)
     buf_200 <- rasterize(buf_200, mask_buf_200)
     buf_500 <- rasterize(buf_500, mask_buf_500)
@@ -141,60 +142,55 @@ foreach(spp_index = 1:length(spp), .packages = c("terra", "tidyverse", "sf", "pu
     diff_cells_200 <- setdiff(buf_cells_200, occ_cells_200)
     
     abs_indices_200 <- sample(diff_cells_200, ifelse(length(diff_cells_200) < nrow(occ_thinned)*10, length(diff_cells_200), nrow(occ_thinned)*10))
-    abs_coords_200 <- xyFromCell(buf_200, abs_indices_200)
+    abs_coords_200 <- as.data.frame(xyFromCell(buf_200, abs_indices_200))
     colnames(abs_coords_200) = c("lon", "lat")
-    
+    abs_coords_200_sf <- st_as_sf(abs_coords_200, coords = c("lon", "lat"), crs = crs(world_mask)) # transform it into sf object to use in thin function
+
     # do the same for the 500 km buffer
     occ_cells_500 <- terra::extract(buf_500, occ_coords, cells = TRUE)[,"cell"]
     buf_cells_500 <- terra::extract(buf_500, crds(buf_500), cells = TRUE)[,"cell"]
     diff_cells_500 <- setdiff(buf_cells_500, occ_cells_500)
-    
+
     abs_indices_500 <- sample(diff_cells_500, ifelse(length(diff_cells_500) < nrow(occ_thinned)*10, length(diff_cells_500), nrow(occ_thinned)*10))
-    abs_coords_500 <- xyFromCell(buf_500, abs_indices_500)
+    abs_coords_500 <- as.data.frame(xyFromCell(buf_500, abs_indices_500))
     colnames(abs_coords_500) = c("lon", "lat")
-    
+    abs_coords_500_sf <- st_as_sf(abs_coords_500, coords = c("lon", "lat"), crs = crs(world_mask)) # transform it into sf object to use in thin function
+
     
     # thin background data
-    abs_thinned_200 <- thin(abs_coords_200, thin_dist = 3000, runs = 10, ncores = 2)
-    abs_thinned_500 <- thin(abs_coords_500, thin_dist = 3000, runs = 10, ncores = 2)
+    abs_thinned_200 <- thin(abs_coords_200_sf, thin_dist = 3000, runs = 10, ncores = 1)
+    abs_thinned_500 <- thin(abs_coords_500_sf, thin_dist = 3000, runs = 10, ncores = 1)
+    
     
     # create output file
     # output map with occurrence and pseudo-absence data
-    pdf(paste0(path_figures,"/presence_absence/nat1_buffer200_",spp[spp_index],".pdf"))
-    plot(world_mask)
-    points(abs_thinned_200, pch = 19, col = "black", cex = .2)
-    points(occ_thinned_200, pch = 19, col = "red", cex = .2)
-    dev.off()
+    # pdf(paste0(path_imp,"figures/presence_absence/nat1_buffer200_",spp[spp_index],".pdf"))
+    # plot(world_mask)
+    # points(abs_thinned_200, pch = 19, col = "black", cex = .2)
+    # points(occ_thinned_200, pch = 19, col = "red", cex = .2)
+    # dev.off()
+    # 
+    # pdf(paste0(path_imp,"figures/presence_absence/nat1_buffer500_",spp[spp_index],".pdf"))
+    # plot(world_mask)
+    # points(abs_thinned_500, pch = 19, col = "black", cex = .2)
+    # points(occ_thinned_500, pch = 19, col = "red", cex = .2)
+    # dev.off()
     
-    pdf(paste0(path_figures,"/presence_absence/nat1_buffer500_",spp[spp_index],".pdf"))
-    plot(world_mask)
-    points(abs_thinned_500, pch = 19, col = "black", cex = .2)
-    points(occ_thinned_500, pch = 19, col = "red", cex = .2)
-    dev.off()
-    
-    # Merge presence and absence coordinates
-    coords_final_nat_200 <- bind_rows(bind_cols(species = spp[spp_index], present = 1, occ_thinned_200),
-                                  bind_cols(species = spp[spp_index], present = 0, abs_thinned_200)) %>%
+    #Merge presence and absence coordinates
+    coords_final_nat_200 <- bind_rows(bind_cols(species = spp[spp_index], present = 1, occ_thinned),
+                                      bind_cols(species = spp[spp_index], present = 0, abs_thinned_200)) %>%
       as.data.frame() %>%
       mutate(status = "Nat")
-    
-    save(coords_final_nat_200, file = paste0(path_imp, "output/coords_final_nat/coords_final_nat_200", spp[spp_index],".RData"))
-   
-    coords_final_nat_500 <- bind_rows(bind_cols(species = spp[spp_index], present = 1, occ_thinned_500),
-                                  bind_cols(species = spp[spp_index], present = 0, abs_thinned_500)) %>%
+
+    save(coords_final_nat_200, file = paste0(path_imp, "output/coords_final_nat/coords_final_nat_200_", spp[spp_index],".RData"))
+
+    coords_final_nat_500 <- bind_rows(bind_cols(species = spp[spp_index], present = 1, occ_thinned),
+                                      bind_cols(species = spp[spp_index], present = 0, abs_thinned_500)) %>%
       as.data.frame() %>%
       mutate(status = "Nat")
-    
-    save(coords_final_nat_500, file = paste0(path_imp, "output/coords_final_nat/coords_final_nat_500", spp[spp_index],".RData"))
-    
-    })} # end of foreach
 
+    save(coords_final_nat_500, file = paste0(path_imp, "output/coords_final_nat/coords_final_nat_500_", spp[spp_index],".RData"))
 
-
-
-
-
-
-
+  })} # end of foreach
 
 
