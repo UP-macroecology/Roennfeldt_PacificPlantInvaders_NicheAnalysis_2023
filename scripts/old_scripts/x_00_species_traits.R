052272c481af1b5420741ed5a9ac02933cf8da4a
Anna Rönnfeldt <roennfeldt@ibb.uni-potsdam.de>
2023-05-25 11:12:31 +0200

    moved and renamed all old scripts

---
+++ scripts/old_scripts/x_00_species_traits.R
@@ -0,0 +1,20 @@
+library(BIEN)
+library(GIFT)
+library(rtry)
+
+# species list 
+load("data/specs_all.RData")
+
+
+# BIEN traits -------------------------------------------------------------
+BIEN_trait_list()
+
+#  GIFT traits ------------------------------------------------------------
+
+t <- GIFT_traits_meta()
+
+
+
+# TRY traits --------------------------------------------------------------
+
+# https://www.try-db.org/de/TabDetails.php
