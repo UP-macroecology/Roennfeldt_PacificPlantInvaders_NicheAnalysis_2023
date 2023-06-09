0adbb0bc8682832180d4d27fdb0d5f70b1c8b1d5
Anna Rönnfeldt <roennfeldt@ibb.uni-potsdam.de>
2023-06-09 16:23:09 +0200

    changed hybrid reformatting again

--- scripts/0_PaciFLora.R
+++ scripts/0_PaciFLora.R
@@ -1,56 +1,47 @@
 library(ape) # to work with phylogenetic trees
 library(tidyverse)
+
 rm(list = ls())
 
 # Species data ------------------------------------------------------------
-
-
 # data set on pacific island invaders from: https://bdj.pensoft.net/article/67318/
 species_names <- read.table("data/PaciFLora.txt", header = TRUE) %>%
   dplyr::select(Species) %>%
   arrange(Species) %>%
   distinct() %>%
   drop_na() %>%
-  rename(species = Species)
-
-
-length(unique(species_names$Island)) # 482
-length(unique(species_names$IslandGroup)) # 50
-
-# compare to current species list
-load("data/specs_all.RData")
-
-t <- setdiff(specs_all, PaciFlora)
-t2 <- setdiff(PaciFlora, specs_all)
+  rename(species_orig = Species) %>%
+  mutate(species_id = row_number(), .before = "species_orig") %>%
+  mutate(species_changed = species_orig) %>% # duplicate species column twice
+  mutate(species_no_x = species_orig)
 
 # 314 Arten aus der ursprünglichen Liste kommen nicht mehr in der neuen vor, dafür 1504 neue. 
 # das kann auch damit zu tun haben, dass die Namen nochmal vereinheitlicht wurden
 
 
-# Paciflora names for hybrids are in a format that causes errors when running the wcvp_distribution() function
-hybrids <- species_names %>%
-  filter(grepl('_x', species)) %>%
-  pull() 
 
-# hybrids_corrected <- data.frame(PaciFlora = str_replace(hybrids, "_x", ""))
-hybrids_corrected <- hybrids %>%
-  str_replace("_x", "") %>%
-  str_replace(" ", " x ")
+# reformat hybrids --------------------------------------------------------
+species_names <- species_names[c(121:123),]
 
 
+for (i in 1:nrow(species_names)){
+  
+  print(i)
+  print(species_names[i,"species_orig"])
 
-# remove hybrids with the old format and add the corrected format
-species_all <- species_names %>%
-  filter(!grepl('_x', species)) %>% # remove old format
-  rbind(data.frame(species = hybrids_corrected)) # add corrected format
+  if (grepl("_x", species_names[i,"species_orig"])) {
+    
+    # move the "x" from the end of the string to between genus and species in the second column
+    species_names[i,"species_changed"] <- gsub(x = species_names[i,"species_changed"], pattern = "_x", replacement = "")
+    species_names[i,"species_changed"] <- gsub(x = species_names[i,"species_changed"], pattern = " ", replacement = " x ")
+    
+    # remove "x" completely from the second column
+    species_names[i,"species_no_x"] <- gsub(x = species_names[i,"species_no_x"], pattern = "_x", replacement = "")
+  } # end of if 
+}
 
 
-
-
-species_all <- cbind(species_names$species , species_all) %>%
-  rename("species_old_format" = "species_names$species")
-
-save(species_all, file = "data/initial_species_list.RData")
+save(species_names, file = "data/initial_species_list.RData")
 
 
 
