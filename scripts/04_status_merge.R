5669228020680877052ca0e4daaecb561d464806
AnnaRoenn <87476282+AnnaRoenn@users.noreply.github.com>
2023-06-14 08:42:20 +0200

    resolved first conflicts

--- scripts/04_status_merge.R
+++ scripts/04_status_merge.R
@@ -1,3 +1,4 @@
+library(conflicted)
 library(tidyverse)
 library(sf)
 
@@ -65,12 +66,22 @@ save(occ_status_all, file = "data/testing/occ_status_all.RData")
 
 load("data/testing/occ_status_all.RData")
 
-occ_status_all <- occ_status_all[sample(10),]
+occ_status_all <- occ_status_all[sample(200),]
 
-# 1. assign final status if sources are in agreement
+# 1. check status for agreements/conflicts between the sources
 
-t <- occ_status_all %>%
-  mutate(final_status = case_when(
+# - native: no conflicts between sources
+# - introduced: no conflicts between sources
+# - unknown: exact status of the species is unknown (could be both)
+# - NA: no status information available 
+# - conflicts: these hightlight conflicts between the different sources. 
+#             Either one source contradicting the other two (e.g., WCVP_vs_rest)
+#             or one source has no info while the other two contradict each other (e.g., W_vs_G_no_Gl)
+
+
+
+occ_status_merged <- occ_status_all %>%
+  mutate(status_check = case_when(
     
     # ID 1
     wcvp_status == "native" & gift_status == "native" & is.na(glonaf_status) ~ "native",
@@ -162,4 +173,91 @@ t <- occ_status_all %>%
     # ID 30
     is.na(wcvp_status) & gift_status == "non-native" & glonaf_status == "alien" ~ "introduced",
     
+    # ID 31
+    is.na(wcvp_status) & gift_status == "naturalized" & is.na(glonaf_status) ~ "introduced",
+    
+    # ID 32
+    is.na(wcvp_status) & gift_status == "naturalized" & glonaf_status == "naturalized" ~ "introduced",
+    
+    # ID 33
+    is.na(wcvp_status) & gift_status == "naturalized" & glonaf_status == "alien" ~ "introduced",
+    
+    # ID 34
+    is.na(wcvp_status) & is.na(gift_status)  & is.na(glonaf_status) ~ NA,
+    
+    # ID 35
+    is.na(wcvp_status) & is.na(gift_status)  & glonaf_status == "naturalized" ~ "introduced",
+    
+    # ID 36
+    is.na(wcvp_status) & is.na(gift_status)  & glonaf_status == "alien" ~ "introduced",
+    
+    # ID 37
+    wcvp_status == "native" & gift_status == "unknown" & is.na(glonaf_status) ~ "native",
+    
+    # ID 38
+    wcvp_status == "native" & gift_status == "unknown"  & glonaf_status == "naturalized" ~ "W_vs_Gl_no_G",
+    
+    # ID 39
+    wcvp_status == "native" & gift_status == "unknown"  & glonaf_status == "alien" ~ "W_vs_Gl_no_G",
+
+    # ID 40
+    wcvp_status == "introduced" & gift_status == "unknown"  & is.na(glonaf_status) ~ "introduced",
+    
+    # ID 41
+    wcvp_status == "introduced" & gift_status == "unknown"  & glonaf_status == "naturalized" ~ "introduced",
+    
+    # ID 42
+    wcvp_status == "introduced" & gift_status == "unknown"  & glonaf_status == "alien" ~ "introduced",
+    
+    # ID 43
+    is.na(wcvp_status) & gift_status == "unknown"  & is.na(glonaf_status) ~ "unknown",
+    
+    # ID 44
+    is.na(wcvp_status) & gift_status == "unknown"  & glonaf_status == "naturalized" ~ "introduced",
+    
+    # ID 45
+    is.na(wcvp_status) & gift_status == "unknown"  & glonaf_status == "alien" ~ "introduced"
   ))
+
+
+
+# resolve conflicts -------------------------------------------------------
+
+# GIFT uses different polygon sizes to assign a status. 
+# If there is a conflict between sources, use the information from the source that refers to a smaller region
+
+# if the area size is not of help, two criteria are used to proceed: 
+# Criterion 1: no source preferences; assign "contradictory"
+# Criterion 2: prefer WCVP over the other two, as it has been targeted towards twdg lvl 3 
+            # (no preferences between GIFT and GloNAF, assign "contradictory")
+
+# there are six distinct conflict types:
+# W_vs_G_no_Gl (no Glonaf info, WCVP and GIFT contradict each other)
+# -> compare area size, and go with the smaller one
+
+# 1. get row index of occurrences with status conflicts
+conflict_index <- which(occ_status_merged$status_check != "native" & occ_status_merged$status_check != "introduced" &
+             occ_status_merged$status_check != "unknown" & !is.na(occ_status_merged$status_check))
+
+for (i in conflict_index) {
+  
+  if (occ_status_merged[i,"status_check"] == "W_vs_G_no_Gl") {
+    
+    # get sizes of the two areas to compare them (both in km^2)
+    areas <- data.frame(source = c("wcvp", "gift"),
+                        area_km2 = c(occ_status_merged[i, "wcvp_area"], occ_status_merged[i, "gift_area"]))
+  
+    smallest_source <- areas[which.min(areas$area_km2),1]
+    
+  } # end if "W_vs_G_no_Gl"
+  
+} # for loop over conflict_index
+
+unique(occ_status_merged$status_check)
+occ_status_merged[t,]
+
+i <- 3
+
+areas <- data.frame(source = c("wcvp_area", "gift_area"),
+                    area_km2 = c(occ_status_merged[i, "wcvp_area"], occ_status_merged[i, "gift_area"]))
+
