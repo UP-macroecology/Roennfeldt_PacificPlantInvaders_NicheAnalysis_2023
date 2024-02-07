6c8b2247821a27edf9f2159a55730f17acbbffa4
Anna Rönnfeldt <roennfeldt@ibb.uni-potsdam.de>
2024-02-07 13:49:39 +0100

    added proper metrics

--- scripts/08_geo_fragmentation_metric.R
+++ scripts/08_geo_fragmentation_metric.R
@@ -1,6 +1,7 @@
 library(sf)
 library(terra)
 library(landscapemetrics)
+library(ggplot2)
 
 rm(list = ls())
 
@@ -13,107 +14,118 @@ chelsa <- rast("Z:/Arbeit/datashare/data/envidat/biophysical/CHELSA_V2/global/CH
 # change values to 1 to decrease size
 values(chelsa) <- 1 
 
+pac_islands <- vect("data/spatial_data/pacific_islands.shp") # island shape files
 
 # load in tdwg regions wwe're working with 
 tdwg <- st_read("data/tdwg/geojson/level1.geojson")[-c(6,9),] # without antarctic
 
-eur_vec <- vect(tdwg[1,3])
 
-eur_rast <- rasterize(eur_vec, chelsa, touches = TRUE)
-# cut raster to extent of the vector
-eur_rast_2 <- crop(eur_rast, ext(eur_vec))
+#df to store results
 
-terra::plot(eur_rast, col = "lightgreen")
-
-terra::plot(vect(tdwg[1,3]), add = TRUE)
-
-terra::lines(ext(eur_vec))
-terra::lines(ext(eur_rast_2), col = "red")
-
-
-
-# metric: CAI_CV, which is a core are metric that is scaled to the mean and thus easily comparable
-
-lsm_c_cai_cv(eur_rast_2)$value
-lsm_c_clumpy(eur_rast_2)
-
+df_fragmentation <- data.frame(region = rep(tdwg$LEVEL1_NAM, each = 4),
+                               metric_name = rep(c("AI","CAI", "CLUMPY", "CORE"), 7),
+                               metric_value = NA)
+#regions <- tdwg$LEVEL1_NAM
 
 # build a loop:
 
-# prepare vector
-# prepare raster and crop it to the extent of the vector
-
-# get landscape metrics for the raster
-# store results in df
-
-
-# example from landscapemetrics package:
-landscape <- terra::rast(landscapemetrics::landscape)
-lsm_c_clumpy(landscape)
-
-
-lsm_c_cai_cv(landscape)
-
-
-
-terra::plot(landscape)
-
-
-
-# old version -------------------------------------------------------------
-
-
-
-
-tdwg <- st_read("data/tdwg/geojson/level1.geojson")[-c(6,9),] # without antarctic
-
-tdwg_poly <- st_cast(tdwg, "POLYGON")
-regions_vect <- vect(tdwg_poly) 
-
-area_all <- expanse(regions_vect, unit="m", transform=TRUE)
-perim_all <- perim(regions_vect) # in m
-
-all_regions <- subset(st_cast(tdwg, "POLYGON"))[,2:3] %>%
-  mutate(area = area_all,
-         perimeter =perim_all)
-
-pac_islands <- vect("data/spatial_data/pacific_islands.shp") # island shape files
-
-area_pac <- sum(expanse(pac_islands, unit="m", transform=TRUE))
-perimeter_pac <- sum(perim(pac_islands)) # in m
-
-
-regions <- unique(tdwg$LEVEL1_NAM)
-
-fragmen_df <- data.frame("region" = NULL,
-                         "area" = NULL,
-                         "perimeter" = NULL,
-                         "fragmen_1" = NULL,
-                         "fragmen_2" = NULL)
-
-for (region in regions) {
+for (i in 1:nrow(tdwg)) {
   
-  region_df <- subset(all_regions, LEVEL1_NAM == region)
+  reg_name <- tdwg$LEVEL1_NAM[i]
+  print(reg_name)
+  reg_vec <- tdwg[i,3]
   
-  area_total <- sum(region_df$area) # in m
-  perimeter_total <- sum(region_df$perimeter)
+  # get extent of vector
+  e <- ext(reg_vec)
   
-  fragmen_df <- rbind(fragmen_df,
-                      data.frame("region" = region,
-                                 "area" = area_total,
-                                 "perimeter" = perimeter_total,
-                                 "fragmen_1" = area_total/perimeter_total,
-                                 "fragmen_2" = perimeter_total/area_total))
-} # for loop over regions
+  # increase extent size
+  e2 <- e
+  e2[1] <- e[1] - 2
+  e2[2] <- e[2] + 2
+  e2[3] <- e[3] - 2
+  e2[4] <- e[4] + 2
+  
+  # landcsape funcitons require SpatRaster as input
+  reg_rast <- rasterize(reg_vec, chelsa, touches = TRUE)
+  # cut raster to modified extent of the vector
+  reg_rast <- crop(reg_rast, ext(e2))
+  
+  # change NA values to 2(not all landscape functions work if there is only one class)
+  reg_rast[is.na(reg_rast)] <- 2
+  
+  # get metrics
+  reg_ai <- lsm_c_ai(reg_rast)[1,6]
+  reg_cai_cv <- lsm_c_cai_cv(reg_rast)[1,6]
+  reg_clumpy <- lsm_c_clumpy(reg_rast)[1,6]
+  reg_core_cv <- lsm_c_core_cv(reg_rast)[1,6]
+  
+  # store in results
+  df_fragmentation[df_fragmentation$region == reg_name & df_fragmentation$metric_name == "AI" , "metric_value"] <- reg_ai
+  df_fragmentation[df_fragmentation$region == reg_name & df_fragmentation$metric_name == "CAI" , "metric_value"] <- reg_cai_cv
+  df_fragmentation[df_fragmentation$region == reg_name & df_fragmentation$metric_name == "CLUMPY" , "metric_value"] <- reg_clumpy
+  df_fragmentation[df_fragmentation$region == reg_name & df_fragmentation$metric_name == "CORE" , "metric_value"] <- reg_core_cv
+}
 
-# add pacific info
 
-fragmen_df <- rbind(fragmen_df,
-                    data.frame("region" = "PACIFIC",
-                               "area" = area_pac,
-                               "perimeter" = perimeter_pac,
-                               "fragmen_1" = area_pac/perimeter_pac,
-                               "fragmen_2" = perimeter_pac/area_pac))
+# prepare metrics or Pacific Islands
+# get extent of vector
+e <- ext(pac_islands)
 
-save(fragmen_df, file = "data/trait_analysis/fragmentation_metric.RData")
+# increase extent size
+e2 <- e
+e2[1] <- e[1] - 2
+e2[2] <- e[2] + 2
+e2[3] <- e[3] - 2
+e2[4] <- e[4] + 2
 
+# landcsape funcitons require SpatRaster as input
+pac_rast <- rasterize(pac_islands, chelsa, touches = TRUE)
+# cut raster to modified extent of the vector
+pac_rast <- crop(pac_rast, ext(e2))
+
+# change NA values to 2 (not all landscape functions work if there is only one class)
+pac_rast[is.na(pac_rast)] <- 2
+
+# get metrics
+pac_ai <- lsm_c_ai(pac_rast)[1,6]
+pac_cai_cv <- lsm_c_cai_cv(pac_rast)[1,6]
+pac_clumpy <- lsm_c_clumpy(pac_rast)[1,6]
+pac_core_cv <- lsm_c_core_cv(pac_rast)[1,6]
+
+df_pac <- data.frame(region = "PACIFIC",
+                     metric_name = c("AI","CAI", "CLUMPY", "CORE"),
+                     metric_value = NA)
+
+df_pac[df_pac$metric_name == "AI", "metric_value"] <- pac_ai
+df_pac[df_pac$metric_name == "CAI", "metric_value"] <- pac_cai_cv
+df_pac[df_pac$metric_name == "CLUMPY", "metric_value"] <- pac_clumpy
+df_pac[df_pac$metric_name == "CORE", "metric_value"] <- pac_core_cv
+
+# combine results
+df_fragmentation <- rbind(df_fragmentation, df_pac)
+
+
+save(df_fragmentation, file = "data/trait_analysis/df_fragmentation_metrics.RData")
+
+
+ggplot(data = subset(df_fragmentation, metric_name == "AI"), aes(x = metric_name, y = metric_value, group = region, color = region)) +
+  geom_line() +
+  geom_point()
+
+ggplot(data = subset(df_fragmentation, metric_name == "CAI"), aes(x = metric_name, y = metric_value, group = region, color = region)) +
+  geom_line() +
+  geom_point()
+
+ggplot(data = subset(df_fragmentation, metric_name == "CORE"), aes(x = metric_name, y = metric_value, group = region, color = region)) +
+  geom_line() +
+  geom_point()
+
+ggplot(data = subset(df_fragmentation, metric_name == "CLUMPY"), aes(x = metric_name, y = metric_value, group = region, color = region)) +
+  geom_line() +
+  geom_point()
+ggplot(data = df_fragmentation, aes(x = metric_name, y = metric_value, group = region, color = region)) +
+  geom_line() +
+  geom_point()
+
+
+# -> I'll play around with including CAI or AI (tendency towards CAI)
