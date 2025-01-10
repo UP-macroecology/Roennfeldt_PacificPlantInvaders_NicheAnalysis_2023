14ac278e27e0402a08a8a1c0832c2a401e1165a9
Anna Rönnfeldt <roennfeldt@ibb.uni-potsdam.de>
2025-01-10 14:03:46 +0100

    first species selection: updated path structures & renamed final output file from "first_selection_species_list" to "spp_first_selection"

--- scripts/03_first_species_selection.R
+++ scripts/03_first_species_selection.R
@@ -13,15 +13,18 @@ library(dplyr)
 library(foreach)
 library(doParallel)
 
-# set paths
 
-path_imp  <- file.path("/import/ecoc9z/data-zurell/roennfeldt/C1/") 
+
+# required path -----------------------------------------------------------
+
+
+path_data  <- file.path("/import/ecoc9z/data-zurell/roennfeldt/C1/") 
 
 # required data -----------------------------------------------------------
-load(paste0(path_imp, "input/occ_count_crit_1.RData")) 
-# load(paste0(path_imp, "input/spp_val.RData"))
 
-# determine the original number of suitable species
+load(paste0(path_data, "/occurrence_data/regional_occs/criterion_1/occ_count_crit_1.RData")) 
+
+# determine the original number of suitable species as a starting point for the loop
 # these are the same species the thinning was done for in the previous two scripts
 occ_count_crit_1 <- occ_count_crit_1 %>% 
   arrange(species) %>%
@@ -40,9 +43,6 @@ spp <- spp_suitable$species
 
 
 # create df for that will store the number of occs per species and region
-# rows: 554 (number of specs in spp)
-# columns: 9 (native, pac, eur, afr, ate, atr, sam, nam)
-
 nr_occs_df <- data.frame(species = spp,
                          nat = 0,
                          pac = 0,
@@ -62,7 +62,7 @@ foreach(spp_index = 1:length(spp), .packages = c("tidyverse")) %do% {
                     
                   # load in native occ file for this species
                   
-                  load(paste0(path_imp, "output/coords_final_nat/coords_final_nat_200_",spp[spp_index],".RData")) # object: coords_final_nat_200 #TODO
+                  load(paste0(path_data, "/occurrence_data/coords_final_nat/coords_final_nat_200_",spp[spp_index],".RData")) # object: coords_final_nat_200 #TODO
                   
                   # get the number of native occurrences and add info to df
                   nr_nat <- nrow(coords_final_nat_200)
@@ -72,7 +72,7 @@ foreach(spp_index = 1:length(spp), .packages = c("tidyverse")) %do% {
                   # regions <- c("pac", "afr", "ate", "atr", "aus", "eur", "nam", "sam")
                   
                   # get the regions for which intr files have been saved for the current species
-                  regions <- list.files(paste0(path_imp, "output/coords_final_intr/"), pattern = spp[spp_index]) %>%  #TODO
+                  regions <- list.files(paste0(path_data, "/occurrence_data/coords_final_intr/"), pattern = spp[spp_index]) %>%  #TODO
                     str_remove(".RData") %>% 
                     str_split(pattern = "_") %>%
                     map(~ .x[[5]]) %>%
@@ -80,7 +80,7 @@ foreach(spp_index = 1:length(spp), .packages = c("tidyverse")) %do% {
                   
                   for (region in regions) {
                     
-                    load(paste0(path_imp, "output/coords_final_intr/coords_final_intr1_200_",region,"_",spp[spp_index],".RData")) #TODO
+                    load(paste0(path_data, "/occurrence_data/coords_final_intr/coords_final_intr1_200_",region,"_",spp[spp_index],".RData")) #TODO
                     
                     # accidentally saved these objects as "nat" instead of "intr"
                     nr_reg <- nrow(coords_final_nat_200)
@@ -91,10 +91,10 @@ foreach(spp_index = 1:length(spp), .packages = c("tidyverse")) %do% {
 })} # end of foreach over spp
 
 
-save(nr_occs_df, file = paste0(path_imp, "output/nr_occs_df_first_selection.RData"))
+save(nr_occs_df, file = paste0(path_data, "/occurrence_data/nr_occs_df_first_selection.RData"))
 
 
-# identify species that fit the final selection criterion:
+# identify species that fit the final selection criterion AFTER the thinnning:
 # - >= 20 occs in the native range
 # - >= 20 intr occs in the pacific region
 # - >= 20 intr occs in at least one of the other regions
@@ -108,5 +108,5 @@ suitable$mainland_regions <- rowSums(suitable[,4:10], na.rm = TRUE)
 spp_final <- suitable[!(suitable$nat == 0 | suitable$pac == 0 | suitable$mainland_regions == 0),]$species
 
 
-save(spp_final, file = paste0(path_imp, "output/first_selection_species_list.RData")) 
+save(spp_final, file = paste0(path_data, "/species_selection/spp_first_selection.RData")) 
 
