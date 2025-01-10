c7d4e13b5d886ae90d8d64f6cd3b789294b6bfb2
Anna Rönnfeldt <roennfeldt@ibb.uni-potsdam.de>
2025-01-10 17:40:01 +0100

    folder structure: initial commit

---
+++ scripts/misc/folder_structure.R
@@ -0,0 +1,11 @@
+#' ---------------------------
+#
+# Purpose of script: checking whether required folder structure exists. Creating missing folders
+# Author: Anna Rönnfeldt
+# Date Created: 2025-01-10
+# Email: roennfeldt@uni-potsdam.de
+#
+# Notes: /
+#
+#' ---------------------------
+
