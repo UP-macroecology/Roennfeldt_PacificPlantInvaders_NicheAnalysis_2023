1a36a952c18ba6622f547a29fda7234532415152
Anna Rönnfeldt <roennfeldt@ibb.uni-potsdam.de>
2025-01-28 10:50:44 +0100

    supp data prep: rounded numeric values

--- scripts/misc/data_supp_prep.R
+++ scripts/misc/data_supp_prep.R
@@ -1,13 +1,10 @@
 library(dplyr)
 
 load("results/ecospat/master_results_AC.RData") # results niche comparison
-load("data/trait_analysis/input_TA_unstand.RData") # trait data 
-load("data/native_region_IDs.RData") # native range IDs
-load("data/climate_zones/spp_zones_all.RData") # native main climate zones
+load("data/trait_data/input_TA_unstand.RData") # trait data 
+load("data/native_regions/native_region_IDs.RData") # native range IDs
+load("data/native_regions/spp_zones_all.RData") # native main climate zones
 
-names(master_results_AC) 
-
-# species, native_region_ID, main_native_climate, non_native_region, overlap, similarity, p_D_shift, p_D_cons, ecospat_ESU, AUSEP, stand_ESU, traits
 
 sup_data <- master_results_AC %>% 
   mutate(total_esu = rel_expansion + rel_stability + rel_unfilling) %>%
@@ -16,25 +13,26 @@ sup_data <- master_results_AC %>%
   mutate(stand_expansion = rel_expansion / total_esu) %>%
   select(species, region, 
          p_D_shift, p_D_cons, similarity, 
+         stand_unfilling, stand_stability, stand_expansion,
          unfilling, stability, expansion, 
-         rel_abandonment, rel_unfilling, rel_stability, rel_expansion, rel_pioneering,
-         stand_unfilling, stand_stability, stand_expansion) %>% 
+         rel_abandonment, rel_unfilling, rel_stability, rel_expansion, rel_pioneering) %>% 
   # join with trait data
   left_join(select(input_TA, species, region, mean_height, mean_seedmass, growth_form, lifecycle, range_size_nat, 
                    niche_breadth_nat, niche_centroid_a_nat, niche_centroid_b_nat, years_since_intro, lat_dist),
             join_by("species", "region")) %>% 
   left_join(native_tags, join_by("species")) %>% 
   left_join(select(spp_zones_all, species, freq_climate), join_by("species")) %>% 
-  rename("non_native_region" = "region",
+  rename("ecospat_unfilling" = "unfilling",
+         "ecospat_stability" = "stability",
+         "ecospat_expansion" = "expansion",
+         "non_native_region" = "region",
          "native_region_ID" = "tag",
-         "main_native_climate" = "freq_climate") 
+         "main_native_climate" = "freq_climate") %>% 
+  mutate(across(where(is.numeric), round, 4))
 
 
 
-# add traits
-
-# names(sup_data)
-
+# change region labels to long form ---------------------------------------
 
 sup_data$non_native_region <- as.character(sup_data$non_native_region)
 
@@ -47,10 +45,6 @@ sup_data[sup_data == "aus"] <- "australasia"
 sup_data[sup_data == "sam"] <- "southern_america"
 sup_data[sup_data == "eur"] <- "europe"
 
-unique(sup_data$non_native_region)
-
-
-str(sup_data)
 
 # save as csv
 write.csv(sup_data, "results/supplementary_data.csv", row.names = FALSE)
